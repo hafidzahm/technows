2bbb8c18144f5676988ce682cd27a3494658c5bf
hafidzahm <hafizhahm123@gmail.com>
2025-03-26 12:15:38 +0700

    feat: Add lazy loading for images in Homepage

--- folder-client/vite-project/page/Homepage.jsx
+++ folder-client/vite-project/page/Homepage.jsx
@@ -1,20 +1,33 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import http from "../helper/http"
 import Navbar from "./Navbar"
+import Card from "../components/Card"
 
 export default function Homepage() {
+    const [news, setNews] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     useEffect(() => {
         fetchData()
     }, [])
 
     async function fetchData() {
+        setIsLoading(true)
         let response = await http.get('/news', {timeout: 10000})
         console.log(response, '<-----fetchData');
+        setNews(response.data)
+        setIsLoading(false)
     }
     return(
         <>
         <Navbar/>
-        <div>Homepage</div>
+        <div className="flex flex-row flex-wrap justify-center w-5xl gap-5 m-auto">
+        {
+            isLoading ? <div>Loading...</div> : news.map(el => {
+                return <Card key={el.key} data={el}/>
+            })
+        }
+        </div>
+        
         </>
     )
 }
