80cddcb2fe89282964e832c376ae054ecab12b6a
hafidzahm <hafizhahm123@gmail.com>
2025-03-27 05:02:27 +0700

    feat: Add functionality to save details to bookmarks

--- folder-client/vite-project/page/Details.jsx
+++ folder-client/vite-project/page/Details.jsx
@@ -4,6 +4,7 @@ import http from "../helper/http";
 export default function Details() {
   let [params, setParams] = useSearchParams();
   let [isLoading, setIsLoading] = useState(false);
+  let [isSaving, setIsSaving] = useState(false);
   let [content, setContent] = useState([]);
   let [title, setTitle] = useState("");
   let navigate = useNavigate();
@@ -33,6 +34,24 @@ export default function Details() {
     console.log(params.get("key"));
     navigate(`/summarize?key=${params.get("key")}`);
   }
+  async function onBookmarks() {
+    try {
+      setIsSaving(true)
+      console.log(params.get("key"));
+      let token = localStorage.getItem('access_token')
+      let response = await http.post(`/bookmarks?key=${params.get("key")}`, {}, {
+       headers: {
+           'Authorization': `Bearer ${token}`
+       },
+       timeout: 10000
+      })
+      console.log(response);  
+      setIsSaving(false)
+      navigate("/bookmarks")
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async function parsingContent() {
     let container = "";
@@ -48,12 +67,21 @@ export default function Details() {
         </div>
       ) : (
         <div className="max-w-2xl m-auto">
+          <div className="flex flex-row gap-3">
           <button
             onClick={onSummarize}
             className=" px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
             Summarize with AI
           </button>
+          <button
+            onClick={onBookmarks}
+            className=" px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300"
+          >
+            {isSaving ? "Memproses..." : "Save to Bookmarks"}
+          </button>
+          </div>
+          
           <div className="text-4xl mt-5">{title}</div>
           <div className=" border border-gray-500 mt-5 bg-gray-100">
             {content?.map((el, index) => {
