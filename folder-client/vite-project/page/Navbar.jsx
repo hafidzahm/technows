00c11d7220a4d66715e1d66f68cfd72fb7953ce1
hafidzahm <hafizhahm123@gmail.com>
2025-03-26 12:18:04 +0700

    chore: Add News link to Navbar component

--- folder-client/vite-project/page/Navbar.jsx
+++ folder-client/vite-project/page/Navbar.jsx
@@ -62,6 +62,12 @@ export default function Navbar() {
               <>
                 <ul className="font-medium flex flex-row p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                   <Link
+                    to={"/"}
+                    className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  >
+                    News
+                  </Link>
+                  <Link
                     to={"/bookmarks"}
                     className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   >
